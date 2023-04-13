c15c04cb1f37d06b82b44a480c4aebc0cf500934
CrisTurx <cristian.turx@gmail.com>
2023-04-13 21:05:57 +0200

    Quiz

---
+++ src/preguntas.js
@@ -0,0 +1,121 @@
+const preguntas = [
+    {
+      titulo: "Which of the following function of String object returns the character at the specified index?",
+      opciones: [
+        { textoRespuesta: "charAt()", isCorrect: true },
+        { textoRespuesta: "charCodeAt()", isCorrect: false },
+        { textoRespuesta: "concat()", isCorrect: false },
+        { textoRespuesta: "indexOf()", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "Which snippet of CSS is commonly used to center a website horizontally?",
+      opciones: [
+        { textoRespuesta: "site-align: center;", isCorrect: false },
+        { textoRespuesta: "margin: 0 auto;", isCorrect: true },
+        { textoRespuesta: "margin: auto 0;", isCorrect: false },
+        { textoRespuesta: "margin: center;", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "What is not an HTML5 element?",
+      opciones: [
+        { textoRespuesta: "blink", isCorrect: true },
+        { textoRespuesta: "section", isCorrect: false },
+        { textoRespuesta: "header", isCorrect: false },
+        { textoRespuesta: "main", isCorrect: false },
+      ],
+    },
+    {
+      titulo: "What is jQuery?",
+      opciones: [
+        { textoRespuesta: "A framework", isCorrect: false },
+        { textoRespuesta: "jQuery?", isCorrect: false },
+        { textoRespuesta: "none of these", isCorrect: false },
+        { textoRespuesta: "A library", isCorrect: true },
+      ],
+    },
+    {
+      titulo: "var a = []; What does 'typeof a' return?",
+        opciones: [
+          { textoRespuesta: "array", isCorrect: false },
+          { textoRespuesta: "string", isCorrect: false },
+          { textoRespuesta: "undefined", isCorrect: false },
+          { textoRespuesta: "object", isCorrect: true },
+        ],
+      },
+    {
+        titulo: "The # symbol specifies that the selector is?",
+        opciones: [
+          { textoRespuesta: "class", isCorrect: false },
+          { textoRespuesta: "tag", isCorrect: false },
+          { textoRespuesta: "instagram hashtag", isCorrect: false },
+          { textoRespuesta: "id", isCorrect: true },
+        ],
+    },
+    {
+        titulo: "What property is used to change the text color of an element?",
+        opciones: [
+          { textoRespuesta: "fontcolor", isCorrect: false },
+          { textoRespuesta: "textcolor", isCorrect: false },
+          { textoRespuesta: "color", isCorrect: true },
+          { textoRespuesta: "paint", isCorrect: false },
+        ],
+      },
+      {
+        titulo: "Which HTML5 element defines navigation links?",
+        opciones: [
+          { textoRespuesta: "nav", isCorrect: true },
+          { textoRespuesta: "ryanAir", isCorrect: false },
+          { textoRespuesta: "links", isCorrect: false },
+          { textoRespuesta: "navigate", isCorrect: true },
+        ],
+      },
+      {
+        titulo: "How do you call the function 'myFunction'?",
+        opciones: [
+          { textoRespuesta: "myFunction", isCorrect: false },
+          { textoRespuesta: "func myFunction()", isCorrect: false },
+          { textoRespuesta: "myFunction()", isCorrect: true },
+          { textoRespuesta: "cmon Func() wake up", isCorrect: false },
+        ],
+      },
+      {
+        titulo: "Which of the following function of Array object removes the last element from an array and returns that element?",
+        opciones: [
+          { textoRespuesta: "push()", isCorrect: false },
+          { textoRespuesta: "pop()", isCorrect: true },
+          { textoRespuesta: "map()", isCorrect: false },
+          { textoRespuesta: "pup()", isCorrect: false },
+        ],
+      },
+      {
+        titulo: "What does the === comparison operator do?",
+        opciones: [
+          { textoRespuesta: "triple equality", isCorrect: false },
+          { textoRespuesta: "It tests for equality of type only", isCorrect: false },
+          { textoRespuesta: "The start of the tunnel", isCorrect: false },
+          { textoRespuesta: "It tests for equality of value and type", isCorrect: true },
+        ],
+      },
+      {
+        titulo: "Lighthouse is a tool for auditing your website. Which choice is not a category of report offered by Lighthouse?",
+        opciones: [
+          { textoRespuesta: "performance", isCorrect: false },
+          { textoRespuesta: "UX design", isCorrect: true },
+          { textoRespuesta: "accessibility", isCorrect: false },
+          { textoRespuesta: "SEO", isCorrect: false },
+        ],
+      },
+      {
+        titulo: "Which choice is not a JavaScript framework?",
+        opciones: [
+          { textoRespuesta: "Angular", isCorrect: false },
+          { textoRespuesta: " Vue", isCorrect: false },
+          { textoRespuesta: "Laravel", isCorrect: true },
+          { textoRespuesta: "React", isCorrect: false },
+        ],
+      },
+  ];
+  
+  export default preguntas;
